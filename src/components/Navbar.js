1ff6a8a430ad86fc44f3dc0e6d9f48485316db8a
Umar Jihad <umarjihad141004@gmail.com>
2023-07-25 11:18:55 +0800

    remove account button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,21 +89,10 @@ function Navbar(){
             <div className="navbar-extra">
                 <a href="/login" className="login-btn">Login</a>
                 <a href="/signup" className="signup-btn">Signup</a>
-                <span href="/" className="account-btn">
-                    <span class="material-symbols-rounded">account_circle</span>
-                </span>
             </div>
 
             {/* mobile */}
             <div className="mobile-menu">
-                <span href="/" className="account-btn-mobile">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                        <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-                        <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
-                    </svg>
-                </span>
                 <div className="humberger-menu" onClick={() => {setShowMobileLink(!showMobileLink)}} ref={mobileMenuBtn}>
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
